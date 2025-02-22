5818be55b213bc9484ec0d45bbce04ee705392a7
Lucy <ziyuzhao1025@gmail.com>
2025-02-22 14:50:33 -0500

    enhance feature functionalities in client p2p behaviours

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -10,10 +10,24 @@ package main
 
 import (
     "fmt"
-    "github.com/yourusername/p2p-networking-tool/internal/nat"
+    "github.com/lucyzhao1997/p2p-networking-tool/internal/nat"
+    "github.com/lucyzhao1997/p2p-networking-tool/internal/security"
+    "github.com/lucyzhao1997/p2p-networking-tool/pkg/config"
+    "log"
 )
 
 func main() {
+
+    cfg := config.LoadConfig()
+
+    // Generate RSA Key Pair
+    privateKey, publicKey, err := security.GenerateKeyPair()
+    if err != nil {
+        log.Fatalf("Failed to generate key pair: %v", err)
+    }
+    fmt.Println("Public Key:", publicKey)
+
+    //get public ip addr
     ip, port, err := nat.GetPublicIP()
     if err != nil {
 		//handle error message
